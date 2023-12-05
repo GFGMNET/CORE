f2792f88940d188b2a8a1c7e06c11c980d1cf9e6
Your Name <you@example.com>
2023-12-05 13:41:16 +0100

    update Views

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const app = express();
+
+
+
+
+// view engine
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+
+// body parse middleware application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+
+//set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get("/", (req, res) => {
+    res.render("home", {
+       title: "Home"  
+     });
+   });
+
+app.listen('80', () => {
+    console.log('Server Started on port');
+});
