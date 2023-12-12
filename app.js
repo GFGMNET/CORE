2320817863c8fe3d0731d0bd03351ce9a7177f76
Your Name <you@example.com>
2023-12-12 11:26:21 +0100

    added postgress dabase with environment variable

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const fs = require('fs');
+require('dotenv').config({ path: '.env' });
+const Pool = require('pg').Pool
 
-
+const pool = new Pool({	
+  host: process.env.DATABASE_PW ,
+  database: 'article',
+  password: process.env.DATABASE_PW ,
+  port: 5432,
+})
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
-
 // body parse middleware application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 // parse application/json
 app.use(bodyParser.json());
@@ -22,11 +26,32 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
+        pool.query('SELECT * FROM articles ORDER BY Date ASC', (error, results) => {
+          if (error) {
+            throw error
+          }
+          res.render("home", {
+            title: "Home" ,
+            posts: results.rows    
+          });
+    
 
-    res.render("home", {
-        title: "Home" 
-        
-      });
+        })
+
+app.get("/article/:id", (req,res)=>{
+    const id = parseInt(req.params.id)
+    pool.query('SELECT * FROM articles WHERE id = $1', [id], (error, results) => {
+        if (error) {
+          throw error
+        }
+        res.render("article"), {
+            title: "News" ,
+            posts: results.rows   
+        }
+      })
+
+})    
+   
 
 })
 app.listen('3080', () => {
