fe11ddb07893daee05fc0b7287be9c62e24d7475
Your Name <you@example.com>
2023-12-12 09:04:51 +0100

    change from MARKDOWN to DB LEVEL

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const fs = require('fs');
+
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+
+// body parse middleware application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// parse application/json
+app.use(bodyParser.json());
+
+//set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+
+    res.render("home", {
+        title: "Home" 
+        
+      });
+
+})
+app.listen('3080', () => {
+    console.log('Server Started on port');
+});
