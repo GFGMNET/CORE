b81a432855552b0c9be2b2ee5f8f51b408530e2e
Your Name <you@example.com>
2023-12-12 09:05:01 +0100

    change to markdown

--- serverless.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-commonjs, import/no-commonjs */
-const { APP_PREFIX } = require('config/env')
-const RecipeBuilder = require('lib/helpers/recipe-builder')
-
-module.exports = new RecipeBuilder()
-  .setService(`${APP_PREFIX}-web`)
-  // TODO
-  .build()
